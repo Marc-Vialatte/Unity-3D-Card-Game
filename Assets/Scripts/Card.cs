d233d04462c5acde78d2148018643606c205da6d
Marc Vialatte <vialatte.marc@gmail.com>
2020-11-20 18:50:14 +0100

    Commit 0.1
    End video 5

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+
+public class Card
+{
+    public int id;
+    public string cardName;
+    public int cost;
+    public int damages;
+    public int movement;
+    public int rangeMin;
+    public int rangeMax;
+    public string cardDescription;
+
+    public Sprite cardImage;
+    
+    public Card() 
+    {
+        
+    }
+
+    public Card(Sprite CardImage)
+    {
+        cardImage = CardImage;
+    }
+
+    public Card(int Id, string CardName, int Cost, int Damages, int Movement, int RangeMin, int RangeMax, string CardDescription, Sprite CardImage)
+    {
+        id = Id;
+        cardName = CardName;
+        cost = Cost;
+        damages = Damages;
+        movement = Movement;
+        rangeMin = RangeMin;
+        rangeMax = RangeMax;
+        cardDescription = CardDescription;
+
+        cardImage = CardImage;
+    }
+}
---
+++ Assets/Scripts/ThisCard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ThisCard : MonoBehaviour
+{
+    public List<Card> thisCard = new List<Card>();
+    public int thisId;
+
+    public int id;
+    public string cardName;
+    public int cost;
+    public int damages;
+    public int movement;
+    public int rangeMin;
+    public int rangeMax;
+    public string cardDescription;
+
+    public Image card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCard[0] = CardDataBase.cardList[thisId];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        id = thisCard[0].id;
+        cardName = thisCard[0].cardName;
+        cost = thisCard[0].cost;
+        damages = thisCard[0].damages;
+        movement = thisCard[0].movement;
+        rangeMin = thisCard[0].rangeMin;
+        rangeMax = thisCard[0].rangeMax;
+        cardDescription = thisCard[0].cardDescription;
+        card.sprite = thisCard[0].cardImage;
+    }
+}
